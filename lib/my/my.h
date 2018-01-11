58f9968639bf16eacc61ec7fe52e13c95dfe3895
arthur <arthur.teisseire@epitech.eu>
2018-01-11 14:12:30 +0100

    Repo inited with libmy

---
+++ lib/my/my.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : my.h
+** File description:
+** By Arthur Teisseire
+*/
+
+#ifndef MY_H
+#define MY_H
+
+#include <unistd.h>
+
+void my_puterror(char *str);
+void my_putstr(char *str);
+int my_strlen(char *str);
+
+#endif
